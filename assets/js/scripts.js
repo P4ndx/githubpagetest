b345d27fea53c2e2718384285abda452ce613bdd
P4ndx <leonardo.herrerias@gmail.com>
2025-02-20 13:31:41 -0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
+});
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  
+  $('#myModal').modal(options)
